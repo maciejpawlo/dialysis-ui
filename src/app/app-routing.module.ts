ccad07367d3143bf48dc13d4dc265b948a881b90
maciejpawlo <maciejpawlo@gmail.com>
2022-06-26 15:05:10 +0200

    Created project folder structure, added Angular Material and Angular Flex Layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContentLayoutComponent } from './modules/layout/content-layout/content-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ContentLayoutComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './modules/shared/material.module';
+import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ToolbarComponent } from './modules/layout/toolbar/toolbar.component';
+import { SidenavComponent } from './modules/layout/sidenav/sidenav.component';
+import { AuthLayoutComponent } from './modules/layout/auth-layout/auth-layout.component';
+import { ContentLayoutComponent } from './modules/layout/content-layout/content-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    SidenavComponent,
+    AuthLayoutComponent,
+    ContentLayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
